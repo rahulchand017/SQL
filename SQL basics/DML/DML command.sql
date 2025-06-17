3033b2591892dec8418977b691297a87359dfd81
Rahul Chand <rahulchand017@outlook.com>
2025-06-17 21:44:33 +0530

    DML

--- SQL basics/DML/DML command.sql
+++ SQL basics/DML/DML command.sql
@@ -37,43 +37,72 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-select * FROM smartphones;
+select * FROM course.smartphones;
 
+-- SELECT all columns
+SELECT * FROM course.smartphones;
 
 
+SELECT model, price, rating FROM smartphones;
 
+SELECT model, battery_capacity, os FROM smartphones;
 
 
+SELECT model as mo, price, rating as pasand FROM smartphones;
 
 
+/* CREATING new column */
 
+SELECT model, 
+SQRT(resolution_width * resolution_width + resolution_height * resolution_height)/ screen_size 
+as ppi FROM smartphones;
 
 
 
+/* Creating a default value */
+SELECT model, 'smartphone' as ADDtype from smartphones;
 
 
 
+/* DISTINCT */
+SELECT DISTINCT brand_name as brands FROM smartphones;
 
+SELECT DISTINCT processor_brand as Processor FROM smartphones;
 
 
 
+/*UNIQUE combination */
 
+SELECT DISTINCT brand_name, processor_brand from smartphones;
 
 
+-- FILTERING rows based on where clause
 
 
+/*FIND all samsung brands */
 
+SELECT * FROM smartphones battery_capacity
+WHERE brand_name = 'samsung';
 
 
+SELECT * FROM smartphones
+WHERE price>50000;
 
 
+SELECT  * FROM smartphones
+WHERE price <21000 AND rating>80;
 
+SELECT * FROM smartphones WHERE brand_name = 'samsung' AND ram_capacity > 8
 
 
+SELECT brand_name FROM smartphones WHERE price>100000;
 
 
 
 
+/*IN and NOT IN*/
+SELECT * FROM smartphones WHERE processor_brand
+IN ('snapdragon', 'exynos','bionic')
 
 
 
@@ -81,44 +110,65 @@ select * FROM smartphones;
 
 
 
+-- Update
 
+UPDATE smartphones SET processor_brand = 'dimensity'
+WHERE processor_brand = 'mediatek';
 
+SELECT * FROM smartphones WHERE processor_brand='mediatek'
 
 
+-- Delete
+DELETE FROM smartphones WHERE price>200000;
 
 
 
 
 
+/* Types of functions in SQL
+1. Builtin 
+a) Scalar
+b) Aggregate (avg)
 
 
+2. User defined
+*/
 
 
+/*
+Aggregate function
+- MAX/MIN
+- AVG
+- COUNT
+- SUM
+- STD
+- VAR
+*/
+SELECT max(price) FROM smartphones;
 
+SELECt max(price) FROM smartphones WHERE brand_name ='samsung';
 
 
+-- Find the total oneplus phones
+SELECT COUNT(brand_name) FROM smartphones WHERE brand_name='oneplus'; 
 
 
+SELECT COUNT(DISTINCT(processor_brand)) FROM smartphones; 
 
 
 
 
+/* Scalar Function
+- ABS (absolute)
+- ROUND
+- CEIL/FLOOR
 
+*/
 
+SELECT  price - 100000 AS 'temp' FROM smartphones;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- ABS
+SELECT  ABS(price - 100000) AS temp FROM smartphones;
 
 
 
