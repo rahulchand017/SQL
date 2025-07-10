d5a0f4a8b8f3ac578ecfec7334da20ff8dbb169a
Rahul Chand <rahulchand017@outlook.com>
2025-07-11 01:03:11 +0530

    Windows

--- Window Function/Windows advance.sql
+++ Window Function/Windows advance.sql
@@ -29,7 +29,30 @@ SELECT * FROM
 
 -- Cumulative average
 
+-- Running Average
+SELECT * FROM (SELECT 
+CONCAT("Match-",CAST(ROW_NUMBER() OVER(ORDER BY ID) AS CHAR)) AS 'match_no',
+SUM(batsman_run) AS 'runs_scored',
+SUM(SUM(batsman_run)) OVER w AS 'career_runs',
+AVG(SUM(batsman_run)) OVER w AS 'career_avg',
+AVG(SUM(batsman_run)) OVER(ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS 'rolling_avg'
+FROM ipl
+WHERE batter = 'V Kohli'
+GROUP BY ID
+WINDOW w AS (ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)) t;
 
 
+-- Percent of Total
+use zomato;
+SELECT f_name,
+(total_value/SUM(total_value) OVER())*100 AS 'percent_of_total'
+FROM (SELECT f_id,SUM(amount) AS 'total_value' FROM orders t1
+JOIN order_details t2
+ON t1.order_id = t2.order_id
+WHERE r_id = 5
+GROUP BY f_id) t
+JOIN food t3
+ON t.f_id = t3.f_id
+ORDER BY (total_value/SUM(total_value) OVER())*100 DESC;
 
 
