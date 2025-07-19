035443b0b62881581f4ed054000c76b1771f55d6
Rahul Chand <rahulchand017@outlook.com>
2025-07-19 21:13:28 +0530

    data cleaning

--- Data cleaning/cleaning.sql
+++ Data cleaning/cleaning.sql
@@ -145,7 +145,122 @@ DROP COLUMN Cpu;
 
 SELECT ScreenResolution, 
 SUBSTRING_INDEX(SUBSTRING_INDEX(ScreenResolution, ' ', -1),'x',1),
+-- This gets the last word in the ScreenResolution string (assuming resolution info is last):
+-- For '1920x1080' → '1920x1080'
 SUBSTRING_INDEX(SUBSTRING_INDEX(ScreenResolution, ' ', -1),'x',-1)
 FROM laptops;
 
 
+ALTER TABLE laptops
+ADD COLUMN resolution_width INTEGER AFTER ScreenResolution,
+ADD COLUMN resolution_height INTEGER AFTER resolution_width;
+
+SELECT * FROM laptops;
+
+UPDATE laptops
+SET
+resolution_width = SUBSTRING_INDEX(SUBSTRING_INDEX(`ScreenResolution`, ' ',-1),'x',1),
+resolution_height = SUBSTRING_INDEX(SUBSTRING_INDEX(`ScreenResolution`, ' ',-1),'x',-1);
+
+ALTER TABLE laptops
+ADD COLUMN touch_screen INTEGER AFTER resolution_height;
+
+SELECT ScreenResolution LIKE '%Touch%' FROM laptops;
+
+UPDATE laptops
+SET touch_screen = `ScreenResolution` LIKE '%Touch%';
+
+ALTER TABLE laptops
+DROP COLUMN ScreenResolution;
+
+SELECT cpu_name,
+SUBSTRING_INDEX(TRIM(cpu_name),' ',2)
+FROM laptops;
+-- TRIM works same as STRIP
+
+UPDATE laptops
+SET cpu_name = SUBSTRING_INDEX(TRIM(cpu_name),' ',2);
+
+SELECT DISTINCT cpu_name FROM laptops;
+
+SELECT Memory FROM laptops;
+
+ALTER TABLE laptops
+ADD COLUMN memory_type VARCHAR(255) AFTER Memory,
+ADD COLUMN primary_storage INTEGER AFTER memory_type,
+ADD COLUMN secondary_storage INTEGER AFTER primary_storage;
+
+SELECT Memory,
+CASE
+	WHEN Memory LIKE '%SSD%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+    WHEN Memory LIKE '%SSD%' THEN 'SSD'
+    WHEN Memory LIKE '%HDD%' THEN 'HDD'
+    WHEN Memory LIKE '%Flash Storage%' THEN 'Flash Storage'
+    WHEN Memory LIKE '%Hybrid%' THEN 'Hybrid'
+    WHEN Memory LIKE '%Flash Storage%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+    ELSE NULL
+END AS 'memory_type'
+FROM laptops;
+
+
+SELECT Memory,
+CASE
+	WHEN Memory LIKE '%SSD%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+    WHEN Memory LIKE '%SSD%' THEN 'SSD'
+    WHEN Memory LIKE '%HDD%' THEN 'HDD'
+    WHEN Memory LIKE '%Flash Storage%' THEN 'Flash Storage'
+    WHEN Memory LIKE '%Hybrid%' THEN 'Hybrid'
+    WHEN Memory LIKE '%Flash Storage%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+    ELSE NULL
+END AS 'memory_type'
+FROM laptops;
+
+
+UPDATE laptops
+SET memory_type = CASE
+	WHEN Memory LIKE '%SSD%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+    WHEN Memory LIKE '%SSD%' THEN 'SSD'
+    WHEN Memory LIKE '%HDD%' THEN 'HDD'
+    WHEN Memory LIKE '%Flash Storage%' THEN 'Flash Storage'
+    WHEN Memory LIKE '%Hybrid%' THEN 'Hybrid'
+    WHEN Memory LIKE '%Flash Storage%' AND Memory LIKE '%HDD%' THEN 'Hybrid'
+    ELSE NULL
+END;
+
+
+SELECT Memory,
+REGEXP_SUBSTR(SUBSTRING_INDEX(Memory,'+',1),'[0-9]+'),
+CASE WHEN Memory LIKE '%+%' THEN REGEXP_SUBSTR(SUBSTRING_INDEX(Memory,'+',-1),'[0-9]+') ELSE 0 END
+FROM laptops;
+
+
+UPDATE laptops
+SET primary_storage = REGEXP_SUBSTR(SUBSTRING_INDEX(Memory,'+',1),'[0-9]+'),
+secondary_storage = CASE WHEN Memory LIKE '%+%' THEN REGEXP_SUBSTR(SUBSTRING_INDEX(Memory,'+',-1),'[0-9]+') ELSE 0 END;
+
+
+
+UPDATE laptops
+SET primary_storage = REGEXP_SUBSTR(SUBSTRING_INDEX(Memory,'+',1),'[0-9]+'),
+secondary_storage = CASE WHEN Memory LIKE '%+%' THEN REGEXP_SUBSTR(SUBSTRING_INDEX(Memory,'+',-1),'[0-9]+') ELSE 0 END;
+
+
+-- primary store has value 1 and 2
+SELECT 
+primary_storage,
+CASE WHEN primary_storage <= 2 THEN primary_storage*1024 ELSE primary_storage END,
+secondary_storage,
+CASE WHEN secondary_storage <= 2 THEN secondary_storage*1024 ELSE secondary_storage END
+FROM laptops;
+
+UPDATE laptops
+SET primary_storage = CASE WHEN primary_storage <= 2 THEN primary_storage*1024 ELSE primary_storage END,
+secondary_storage = CASE WHEN secondary_storage <= 2 THEN secondary_storage*1024 ELSE secondary_storage END;
+
+SELECT * FROM laptops;
+
+ALTER TABLE laptops DROP COLUMN Memory;
+
+ALTER TABLE laptops DROP COLUMN gpu_name;
+
+SELECT * FROM laptops;
